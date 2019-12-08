fe54f90f3990d2e8e9ea86d67ad64edad0dc4e64
asinhacode <57113053+asinhacode@users.noreply.github.com>
2019-12-07 20:25:29 -0500

    5 more questions

--- CodingBat/Logic1.java
+++ CodingBat/Logic1.java
@@ -58,3 +58,56 @@ public int sortaSum(int a, int b) {
   else
   return sum;
 }
+
+public String alarmClock(int day, boolean vacation) {
+  
+  if(vacation)
+  {
+    if(day == 6 || day == 0)
+    return "off";
+    
+    else
+    return "10:00";
+  }
+  
+  else
+  {
+    if(day == 6 || day == 0)
+    return "10:00";
+    
+    else
+    return "7:00";
+  }
+}
+
+
+public boolean love6(int a, int b) {
+  
+  return (a == 6 || b == 6 || a + b == 6 || (Math.abs(a - b) == 6));
+  
+}
+
+public boolean in1To10(int n, boolean outsideMode) {
+  return (outsideMode) ? (n <= 1 || n >= 10) : (n > 0 && n < 11);
+}
+
+public boolean specialEleven(int n) { 
+  return (n % 11 == 0 || n % 11 == 1); 
+}
+
+public boolean more20(int n) {
+  
+  return (n % 20 == 1) || (n % 20 == 2);
+}
+
+public boolean old35(int n) {
+  
+  return ((n % 5 == 0) != (n % 3 == 0));
+  
+}
+
+public boolean less20(int n) {
+  
+  return (n % 20 == 18) || (n % 20 == 19);
+}
+
