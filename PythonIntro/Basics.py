018ebef30bc40ad7fbaf8fd3fe8da7dc39b24139
asinhacode <57113053+asinhacode@users.noreply.github.com>
2019-12-12 14:18:30 -0500

    Update Basics.py

--- PythonIntro/Basics.py
+++ PythonIntro/Basics.py
@@ -9,3 +9,9 @@ character_age = "35"
 
 print("There once was a man name " + character_name)
 print("He was " + character_age + " years old.")
+
+# Types of variable
+character_name = "Sinha"
+character_age = 28
+character_height = 5.10
+isMale = True
