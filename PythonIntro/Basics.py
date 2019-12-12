c247a7e731f8881f8fd85b7df9862264e6bcc0dc
asinhacode <57113053+asinhacode@users.noreply.github.com>
2019-12-12 14:15:05 -0500

    variable declaration

--- PythonIntro/Basics.py
+++ PythonIntro/Basics.py
@@ -1,1 +1,11 @@
 print("Hello World")
+print(2+3)
+
+
+# cannot mix int and character
+# variable declaration
+character_name = "John"
+character_age = "35"
+
+print("There once was a man name " + character_name)
+print("He was " + character_age + " years old.")
