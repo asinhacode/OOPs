00578bce5c1a391b1a7984d489e08501886a2780
asinhacode <57113053+asinhacode@users.noreply.github.com>
2019-12-20 20:54:39 -0500

    6,7,8 and 9

--- CodingBat/Array-2.java
+++ CodingBat/Array-2.java
@@ -66,3 +66,94 @@ public int sum13(int[] nums) {
           return sum;
 }
 
+// 5
+
+public int sum67(int[] nums) 
+{
+  int sum = 0;
+  
+    for(int i = 0; i < nums.length; i++)
+    {
+      if(nums[i] != 6)
+      {
+        sum += nums[i];
+      }
+      
+      else
+      {
+        while(nums[i] != 7)
+        i++;
+      }
+    }
+    
+    return sum;
+}
+
+// 6 
+
+public boolean has22(int[] nums) 
+{
+  boolean two = false;
+  
+    for(int i = 0; i < nums.length; i++)
+    {
+      if(nums[i] == 2)
+      {
+        if(++i < nums.length && nums[i] == 2)
+          two = true;
+      }
+    }
+    
+    return two;
+}
+
+// 7
+public boolean lucky13(int[] nums) 
+{
+
+  for(int i : nums)
+  {
+    if(i == 1 || i == 3)
+    {
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+// 8
+public boolean sum28(int[] nums) 
+{
+  int e = 0;
+  
+  for(int i: nums)
+  {
+    if(i == 2)
+    {
+      e += i;
+    }
+  }
+  
+  return e == 8;
+}
+
+// 9
+public boolean more14(int[] nums) {
+  int o = 0;
+  int f = 0;
+  
+  for(int i : nums)
+  {
+    if(i == 1)
+    o++;
+    
+    if(i == 4)
+    f++;
+  }
+  
+  return o > f;
+}
+
+// 10
+
